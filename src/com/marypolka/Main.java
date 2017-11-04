c25ae2cf1dda2f8aa9e7d22c29a07f851a59f9fb
mariyamarinova <mariya.marinova@yahoo.com>
2017-11-04 21:22:28 +0100

    - updated the main menu; - added the method for selecting the operation; - added the sum method in Utilities;

--- src/com/marypolka/Main.java
+++ src/com/marypolka/Main.java
@@ -1,9 +1,31 @@
 package com.marypolka;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         Utilities.showMenu();
+        int userInput = scanner.nextInt();
+        selectOperation(userInput);
 
+    }
+
+    private static void selectOperation(int userInput) {
+        switch (userInput) {
+            case 1:
+                System.out.print("You choose to sum two digits. Please enter the first one: ");
+                int firstNumber = scanner.nextInt();
+                System.out.println();
+                System.out.print("Please enter the second one: ");
+                int secondNumber = scanner.nextInt();
+                System.out.println();
+                System.out.println("The result is: " + Utilities.sumNumbers(firstNumber, secondNumber));
+                break;
+            default:
+                System.out.println("Invalid operation!");
+        }
     }
 }
--- src/com/marypolka/Utilities.java
+++ src/com/marypolka/Utilities.java
@@ -10,5 +10,11 @@ public class Utilities {
         System.out.println("Menu");
         System.out.println("*******************************");
         System.out.println("1. Sum two numbers");
+        System.out.println("----------------------");
+        System.out.println("Enter the number of the desired operation.");
+    }
+
+    public static int sumNumbers(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
     }
 }
