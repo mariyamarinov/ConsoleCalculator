12ca481042d01c1dac5a4f1ca8eb1b9f3c021e0b
mariyamarinova <mariya.marinova@yahoo.com>
2017-11-04 21:03:34 +0100

    added Utilities class + method for displaying the main menu;

--- src/com/marypolka/Main.java
+++ src/com/marypolka/Main.java
@@ -3,6 +3,7 @@ package com.marypolka;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Utilities.showMenu();
+
     }
 }
---
+++ src/com/marypolka/Utilities.java
@@ -0,0 +1,14 @@
+package com.marypolka;
+
+public class Utilities {
+
+    public static void showMenu() {
+        System.out.println("Console calculator");
+        System.out.println("---------------------");
+        System.out.println("Choose operation from the menu");
+        System.out.println("*******************************");
+        System.out.println("Menu");
+        System.out.println("*******************************");
+        System.out.println("1. Sum two numbers");
+    }
+}
